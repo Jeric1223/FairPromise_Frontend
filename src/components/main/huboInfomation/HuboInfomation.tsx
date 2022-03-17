e127bdf5592cef20fa6bea5d780af01c5905ceb0
JaeHyun <soehd0889@naver.com>
2022-03-17 23:07:07 +0900

    ADD Text Container

---
+++ src/components/main/huboInfomation/HuboInfomation.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import * as S from './style';
+import { huboList } from '../../../constance/data';
+
+const HuboInfomation: FC = () => {
+  return (
+    <S.TextContainer>
+      <h1>후보들 공약확인하기</h1>
+    </S.TextContainer>
+  );
+};
+
+export default HuboInfomation;
