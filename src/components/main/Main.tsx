348c6a86f30bd45dce5987934370561225041d63
JaeHyun <soehd0889@naver.com>
2022-03-17 15:04:20 +0900

    ✏️ css setting

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import Banner from '../banner/Banner';
+import Header from '../header/Header';
 
 const Main: FC = () => {
   return (
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,6 +3,9 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}; 
+  body, html {
+    width: 100%;
+  }
 `;
 
 export default GlobalStyle;
