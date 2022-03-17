ca37e8a3a686d94aa4901640758a7bec206f2627
JaeHyun <soehd0889@naver.com>
2022-03-17 12:02:29 +0900

    font 적용하기

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -3,34 +3,6 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}; 
-
-  @font-face {
-  font-family: 'NanumSquare';
-  font-style: normal;
-  font-weight: 100;
-  src: url("styles/fonts/NanumSquare_acL.ttf")
-}
-
-@font-face {
-  font-family: 'NanumSquare';
-  font-style: normal;
-  font-weight: normal;
-  src: url("styles/fonts/NanumSquare_acR.ttf")
-}
-
-@font-face {
-  font-family: 'NanumSquare';
-  font-style: normal;
-  font-weight: 500;
-  src: url("styles/fonts/NanumSquare_acB")
-}
-
-@font-face {
-  font-family: 'NanumSquare';
-  font-style: normal;
-  font-weight: bold;
-  src: url("styles/fonts/NanumSquare_acEB.ttf")
-}
 `;
 
 export default GlobalStyle;
