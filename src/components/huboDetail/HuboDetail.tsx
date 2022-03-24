f1a3063b21c13862fc03c2501ee847641d907d4f
JaeHyun <soehd0889@naver.com>
2022-03-24 20:20:02 +0900

    map함수 이용해서 퍼블리싱

--- src/components/huboDetail/HuboDetail.tsx
+++ src/components/huboDetail/HuboDetail.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react';
-import * as S from './style';
 import { useLocation } from 'react-router-dom';
-import qs from 'qs';
+import Header from '../header/Header';
+import SloganBanner from './sloganBanner/SloganBanner';
+import { huboList } from '../../constance/data';
+import QueryString from 'qs';
 
 const HuboDetail: FC = () => {
   const location = useLocation();
-  const query = qs.parse(location.search);
+  const queryData = QueryString.parse(location.search, { ignoreQueryPrefix: true });
 
-  console.log(query);
+  console.log(queryData);
 
-  return <h1></h1>;
+  return (
+    <>
+      <Header theme={'black'}></Header>
+      {huboList.map((value, index) => {
+        if (String(value.giho) === queryData.id) {
+          return <SloganBanner key={index} huboinfo={value} />;
+        }
+      })}
+    </>
+  );
 };
 
 export default HuboDetail;
