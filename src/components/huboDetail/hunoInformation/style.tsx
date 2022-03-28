43a00acae814cbbf963f9eca6255ce3e73268729
JaeHyun <soehd0889@naver.com>
2022-03-28 18:22:56 +0900

    style

--- src/components/huboDetail/hunoInformation/style.tsx
+++ src/components/huboDetail/hunoInformation/style.tsx
@@ -29,10 +29,41 @@ export const InfomationContainer = styled.article`
   height: 380px;
   background-color: #f4f5fa;
   margin: 30px 0 100px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   & > #line {
     width: 1000px;
     height: 2px;
     background-color: #dbdce1;
+  }
+`;
+
+export const TextFlexBox = styled.div`
+  padding-left: 40px;
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 55px 0j;
+  margin: 20px 0 40px 0;
+`;
+
+export const TextDetail = styled.div`
+  width: 450px;
+  display: flex;
+  align-items: center;
+
+  & > h1 {
+    font-family: NanumSquare B;
+    font-size: 22px;
+  }
+
+  & > p {
+    font-family: NanumSquare R;
+    font-size: 22px;
+    color: rgba(00, 00, 00, 70%);
+    margin-left: 20px;
   }
 `;
