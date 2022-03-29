25e52383c8c653ed95ece78b2d2e4da7957392a8
JaeHyun <soehd0889@naver.com>
2022-03-29 09:00:26 +0900

    feature 후보자 프로필 확인 페이지

--- src/components/huboDetail/hunoInformation/HuboInformation.tsx
+++ src/components/huboDetail/hunoInformation/HuboInformation.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import * as S from './style';
 import TextDetail from './TextDetail';
 
@@ -8,7 +8,19 @@ interface Props {
 
 const HuboInformation: FC<Props> = (props) => {
   const { huboinfo } = props;
-  console.log(huboinfo);
+  const [birthday, setBirthday] = useState<string>('');
+
+  const birthdaySlice = (string: string) => {
+    let year = string.substring(0, 4);
+    let month = string.substring(4, 6);
+    let day = string.substring(6, 8);
+    return `${year}.${month}.${day}.`;
+  };
+
+  useEffect(() => {
+    setBirthday(birthdaySlice(huboinfo.BIRTHDAY));
+  }, []);
+
   return (
     <>
       <S.FlexCenterWrapper>
@@ -20,10 +32,10 @@ const HuboInformation: FC<Props> = (props) => {
           <S.TextFlexBox>
             <TextDetail title={'이름'} text={`${huboinfo.NAME}(${huboinfo.HANJA_NAME})`} />
             <TextDetail title={'사저'} text={`${huboinfo.ADDR}`} />
-            <TextDetail title={'이름'} text="윤석열" />
-            <TextDetail title={'이름'} text="윤석열" />
-            <TextDetail title={'이름'} text="윤석열" />
-            <TextDetail title={'이름'} text="윤석열" />
+            <TextDetail title={'직업'} text={`${huboinfo.JOB}`} />
+            <TextDetail title={'학력'} text={`${huboinfo.EDU}`} />
+            <TextDetail title={'출생'} text={`${birthday} (${huboinfo.AGE}세)`} />
+            <TextDetail title={'경력'} text={`${huboinfo.CAREER1}\n${huboinfo.CAREER2}`} />
           </S.TextFlexBox>
           <div id="line" />
         </S.InfomationContainer>
