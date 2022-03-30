b4907900be64c7c997c0ea15181757fcf53e1b2a
JaeHyun <soehd0889@naver.com>
2022-03-30 19:42:31 +0900

    feature 후보자 변경 버튼 퍼블리싱

--- src/components/huboDetail/sloganBanner/style.tsx
+++ src/components/huboDetail/sloganBanner/style.tsx
@@ -42,7 +42,7 @@ export const RightWrapper = styled.div<gihoProps>`
   width: 35%;
   height: 100%;
   position: relative;
-  h1 {
+  & > h1 {
     font-family: Typo_S;
     font-size: ${(props) => (props.giho >= 10 ? '22rem' : '25rem')};
     position: absolute;
@@ -50,6 +50,25 @@ export const RightWrapper = styled.div<gihoProps>`
     bottom: -12px;
     left: ${(props) => (props.giho >= 10 ? '-161px' : '-91px')};
     color: white;
+  }
+
+  & > button {
+    width: 180px;
+    height: 43px;
+    background-color: rgba(0, 0, 0, 0.4);
+    border-radius: 18px;
+    font-size: 22px;
+    color: white;
+    font-family: NanumSquare EB;
+    transition: 0.2s;
+    z-index: 100;
+    position: absolute;
+    bottom: 22px;
+    right: 12px;
+  }
+
+  & > button:hover {
+    color: #37e8a0;
   }
 `;
 
@@ -61,7 +80,7 @@ export const HuboImageList = styled.div<gihoProps>`
   right: ${(props) => (props.giho >= 10 ? '0px' : '144px')};
   z-index: 100;
 
-  img {
+  & > img {
     width: 100%;
     height: 100%;
   }
