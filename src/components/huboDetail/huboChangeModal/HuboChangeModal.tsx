6b00e05a8a09a517d5204ce926398eb9d19868f9
JaeHyun <soehd0889@naver.com>
2022-03-30 20:25:03 +0900

    feature 후보 변경하기 모달 퍼블리싱

---
+++ src/components/huboDetail/huboChangeModal/HuboChangeModal.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import * as S from './style';
+
+const HuboChangeModal: FC = () => {
+  return (
+    <S.Container>
+      <S.Background />
+      
+    </S.Container>
+  );
+};
+
+export default HuboChangeModal;
