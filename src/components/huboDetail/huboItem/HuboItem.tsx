7db2e1c4c053aca3250bbff43ff9d0a364519d6b
JaeHyun <soehd0889@naver.com>
2022-04-04 09:46:05 +0900

    huboItem 퍼블리싱 추가

--- src/components/huboDetail/huboItem/HuboItem.tsx
+++ src/components/huboDetail/huboItem/HuboItem.tsx
@@ -1,8 +1,42 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import * as S from './style';
+import QueryString from 'qs';
+import { useLocation } from 'react-router-dom';
 
-const HuboItem: FC = () => {
-  return <></>;
+interface Props {
+  data: {
+    huboid: string;
+    giho: number;
+    name: string;
+    jd_name: string;
+    color: string;
+    slogan: string;
+    report: string;
+  };
+}
+
+const HuboItem: FC<Props> = (props) => {
+  const { data } = props;
+  const [opacity, setOpacity] = useState(false);
+
+  const location = useLocation();
+  const queryData = QueryString.parse(location.search, { ignoreQueryPrefix: true });
+
+  useEffect(() => {
+    if (String(data.giho) === queryData.id) {
+      setOpacity(true);
+    } else {
+      setOpacity(false);
+    }
+  }, [data.giho, queryData.id]);
+
+  return (
+    <S.HuboItemWrapper opacity={opacity ? 1 : 0}>
+      <S.HuboImageWrapper color={data.color}>
+        <img src={require(`assets/image/${data.name}.svg`)} alt="후보자 이미지" />
+      </S.HuboImageWrapper>
+    </S.HuboItemWrapper>
+  );
 };
 
 export default HuboItem;
--- src/components/huboDetail/huboItem/style.tsx
+++ src/components/huboDetail/huboItem/style.tsx
@@ -1,1 +1,33 @@
 import styled from 'styled-components';
+
+export const HuboItemWrapper = styled.div<{ opacity: number }>`
+  width: 142px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  opacity: ${(props) => (props.opacity === 1 ? '1' : '0.5')};
+  flex-wrap: wrap;
+  transition: 0.3s;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const HuboImageWrapper = styled.div<{ color: string }>`
+  width: 100%;
+  height: 167px;
+  border-radius: 27px;
+  background-color: ${(props) => `${props.color}D9`};
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  cursor: pointer;
+
+  img {
+    object-fit: cover;
+    width: 115px;
+    height: 160px;
+    border-radius: 10px;
+  }
+`;
