9aaea291cf58699f49d12bbe2c51c02c5aee9d7a
JaeHyun <soehd0889@naver.com>
2022-04-04 13:55:37 +0900

    hubo Item useNavigate setting

--- src/components/huboDetail/huboItem/HuboItem.tsx
+++ src/components/huboDetail/huboItem/HuboItem.tsx
@@ -1,7 +1,9 @@
 import { FC, useEffect, useState } from 'react';
 import * as S from './style';
 import QueryString from 'qs';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { HuboChangeModalIsOpen } from 'module/atom/huboDetail/huboDetail';
+import { useSetRecoilState } from 'recoil';
 
 interface Props {
   data: {
@@ -18,8 +20,10 @@ interface Props {
 const HuboItem: FC<Props> = (props) => {
   const { data } = props;
   const [opacity, setOpacity] = useState(false);
+  const setIsOpen = useSetRecoilState(HuboChangeModalIsOpen);
 
   const location = useLocation();
+  const navigate = useNavigate();
   const queryData = QueryString.parse(location.search, { ignoreQueryPrefix: true });
 
   useEffect(() => {
@@ -31,10 +35,19 @@ const HuboItem: FC<Props> = (props) => {
   }, [data.giho, queryData.id]);
 
   return (
-    <S.HuboItemWrapper opacity={opacity ? 1 : 0}>
+    <S.HuboItemWrapper
+      onClick={() => {
+        navigate(`/hubo-detail?id=${data.giho}`);
+        setIsOpen(false);
+      }}
+      opacity={opacity ? 1 : 0}
+      color={data.color}
+    >
       <S.HuboImageWrapper color={data.color}>
         <img src={require(`assets/image/${data.name}.svg`)} alt="후보자 이미지" />
       </S.HuboImageWrapper>
+      <span>{data.jd_name}</span>
+      <p>{data.name}</p>
     </S.HuboItemWrapper>
   );
 };
--- src/components/huboDetail/huboItem/style.tsx
+++ src/components/huboDetail/huboItem/style.tsx
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 
-export const HuboItemWrapper = styled.div<{ opacity: number }>`
+export const HuboItemWrapper = styled.div<{ opacity: number; color: string }>`
   width: 142px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  opacity: ${(props) => (props.opacity === 1 ? '1' : '0.5')};
+  opacity: ${(props) => (props.opacity === 1 ? '1' : '0.6')};
   flex-wrap: wrap;
   transition: 0.3s;
+  align-items: center;
 
   &:hover {
     opacity: 1;
+  }
+
+  & > span {
+    font-family: NanumSquare B;
+    font-size: 18px;
+    color: ${(props) => `${props.color}D9`};
+    margin-top: 10px;
+  }
+
+  & > p {
+    font-family: NanumSquare EB;
+    font-size: 20px;
+    margin-top: 4px;
   }
 `;
 
