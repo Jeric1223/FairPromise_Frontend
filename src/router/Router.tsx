3a3826d092e7ebcdc87cbd19f38f3f8e217dd621
JaeHyun <soehd0889@naver.com>
2022-04-12 13:32:55 +0900

    공약 라우터 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../components/main/Main';
 import HuboDetail from '../components/huboDetail/HuboDetail';
+import HuboPromise from 'components/hunoPromise/HuboPromise';
 
 const Router: FC = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const Router: FC = (): JSX.Element => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/hubo-detail" element={<HuboDetail />} />
+        <Route path="/hubo-promise" element={<HuboPromise />} />
       </Routes>
     </BrowserRouter>
   );
