576ce7b8cf23506d504c9f43a2937737dfd1e9d1
JaeHyun <soehd0889@naver.com>
2022-04-15 10:07:57 +0900

    후보 공약 비교하기 페이지 헤더 추가

--- src/components/hunoPromise/HuboPromise.tsx
+++ src/components/hunoPromise/HuboPromise.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react';
+import Header from 'components/header/Header';
+import HuboVSBanner from './huboVSBanner/HuboVSBanner';
 
 const HuboPromise: FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header theme="black" zIndex={100} />
+      <HuboVSBanner />
+    </>
+  );
 };
 
 export default HuboPromise;
