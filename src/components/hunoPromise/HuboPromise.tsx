9ca95a92c4c231f0ef83d38eefa890e85ed5b254
JaeHyun <soehd0889@naver.com>
2022-04-19 20:03:41 +0900

    ADD API REQUEST TEST CODE

--- src/components/hunoPromise/HuboPromise.tsx
+++ src/components/hunoPromise/HuboPromise.tsx
@@ -1,8 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import Header from 'components/header/Header';
 import HuboVSBanner from './huboVSBanner/HuboVSBanner';
+import { Request } from 'util/api/huboPromiseApi';
 
 const HuboPromise: FC = () => {
+  useEffect(() => {
+    Request('/hubolist')
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Header theme="black" zIndex={100} />
