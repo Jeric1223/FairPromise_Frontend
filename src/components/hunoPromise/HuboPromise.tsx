99e7b1e7ff61d26f31551ef3e5575697aabfddd9
JaeHyun <soehd0889@naver.com>
2022-04-22 09:26:02 +0900

    delete test code

--- src/components/hunoPromise/HuboPromise.tsx
+++ src/components/hunoPromise/HuboPromise.tsx
@@ -1,19 +1,8 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import Header from 'components/header/Header';
 import HuboVSBanner from './huboVSBanner/HuboVSBanner';
-import { Request } from 'util/api/huboPromiseApi';
 
 const HuboPromise: FC = () => {
-  useEffect(() => {
-    Request('/hubolist')
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <>
       <Header theme="black" zIndex={100} />
