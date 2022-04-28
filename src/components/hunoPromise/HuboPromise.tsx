aa505eb9d0256682bfd067c782fdd059ff9fe251
JaeHyun <soehd0889@naver.com>
2022-04-28 13:41:41 +0900

    후보 비교하기 페이지 header z-index change

--- src/components/hunoPromise/HuboPromise.tsx
+++ src/components/hunoPromise/HuboPromise.tsx
@@ -1,11 +1,33 @@
-import { FC } from 'react';
+import { FC, useState, useMemo, useEffect } from 'react';
 import Header from 'components/header/Header';
 import HuboVSBanner from './huboVSBanner/HuboVSBanner';
+import { throttle } from 'lodash';
 
 const HuboPromise: FC = () => {
+  const [zIndex, setZIndex] = useState(10);
+
+  const handlerScroll = useMemo(
+    () =>
+      throttle(() => {
+        if (window.scrollY >= 10) {
+          setZIndex(101);
+        } else if (window.scrollY < 10) {
+          setZIndex(10);
+        }
+      }, 200),
+    [],
+  );
+
+  useEffect(() => {
+    window.addEventListener('scroll', handlerScroll);
+    return () => {
+      window.removeEventListener('scroll', handlerScroll);
+    };
+  }, [handlerScroll]);
+
   return (
     <>
-      <Header theme="black" zIndex={100} />
+      <Header theme="black" zIndex={zIndex} />
       <HuboVSBanner />
     </>
   );
