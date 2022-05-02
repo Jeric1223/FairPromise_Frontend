2e8c13b872fe524fbe5c746078243f8e0c18837c
JaeHyun <soehd0889@naver.com>
2022-05-02 13:42:15 +0900

    배너 배치 styled component props 추가

--- src/components/hunoPromise/huboVSBanner/style.tsx
+++ src/components/hunoPromise/huboVSBanner/style.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-export const BannerMainContainer = styled.div`
+export const BannerMainContainer = styled.div<{ reverse: boolean }>`
   width: 100%;
   height: 480px;
   display: flex;
   align-items: center;
   position: relative;
+  flex-direction: ${(props) => (props.reverse === true ? 'row-reverse' : 'row')};
 
   & > img {
     width: 250px;
