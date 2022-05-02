f0763a096eb95eeb1cb0cccaa338845ac105f7b9
JaeHyun <soehd0889@naver.com>
2022-05-02 13:41:51 +0900

    배너 배치 로직 추가

--- src/components/hunoPromise/huboVSBanner/HuboVSBanner.tsx
+++ src/components/hunoPromise/huboVSBanner/HuboVSBanner.tsx
@@ -1,27 +1,36 @@
-import { FC, useEffect } from 'react';
-import * as S from './style';
+import { FC, useEffect, useState } from 'react';
 import HuboVSContainer from '../huboVSContainer/HuboVSContainer';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { VS } from 'assets/index';
+import { strongHubo } from 'constance/strongCandidate';
+import * as S from './style';
 
 interface Props {}
 
 const HuboVSBanner: FC<Props> = () => {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  const [reverse, setReverse] = useState(false);
 
   const hubo_1 = searchParams.get('hubo_1');
   const hubo_2 = searchParams.get('hubo_2');
 
   useEffect(() => {
-    navigate('/hubo-promise?hubo_1=1&hubo_2=2');
     console.log(hubo_1, hubo_2);
-  }, [navigate]);
+    if (hubo_1 > hubo_2) {
+      setReverse(true);
+    }
+  }, [hubo_1, hubo_2, navigate]);
 
   return (
-    <S.BannerMainContainer>
-      <HuboVSContainer color={'#3F69FF'} giho={hubo_1} direction={'left'} />
-      <HuboVSContainer color={'#E61E2B'} giho={hubo_2} direction={'right'} />
+    <S.BannerMainContainer reverse={reverse}>
+      {strongHubo.map((item, index) => {
+        if (String(item.giho) === hubo_1) {
+          return <HuboVSContainer color={item.color} giho={hubo_1} direction={'left'} />;
+        } else if (String(item.giho) === hubo_2) {
+          return <HuboVSContainer color={item.color} giho={hubo_2} direction={'right'} />;
+        }
+      })}
       <img src={VS} alt="VS_image" />
     </S.BannerMainContainer>
   );
