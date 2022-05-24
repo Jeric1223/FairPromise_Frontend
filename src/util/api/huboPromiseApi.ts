78eaa132c63626af6a9f21867f179554a03c1b45
JaeHyun <soehd0889@naver.com>
2022-05-24 15:56:08 +0900

    test code 삭제

--- src/util/api/huboPromiseApi.ts
+++ src/util/api/huboPromiseApi.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-console.log(BASE_URL);
-
 export const Request = (url: string) => {
   return axios
     .get(`${BASE_URL}${url}`)
