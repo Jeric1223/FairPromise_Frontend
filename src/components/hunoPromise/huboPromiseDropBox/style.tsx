fb324553447a3917b5e0c9a768318e5282336792
JaeHyun <soehd0889@naver.com>
2022-05-24 15:57:01 +0900

    드롭 박스 css 설정

--- src/components/hunoPromise/huboPromiseDropBox/style.tsx
+++ src/components/hunoPromise/huboPromiseDropBox/style.tsx
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
 import { fonts } from 'styles/theme';
 
-export const PromiseFlexBox = styled.div`
+export const PromiseFlexBox = styled.div<{ isOpenDropBox: boolean }>`
   width: 100%;
   display: flex;
+  flex-direction: column;
+  overflow: ${(props) => props.isOpenDropBox === false && 'hidden'};
+  transition: 0.3s;
 `;
 
-export const LeftSidePromiseWrapper = styled.div`
-  width: 42%;
-`; 
+export const PromiseWrapper = styled.div<{ isOpenDropBox: boolean }>`
+  display: flex;
+  height: ${(props) => (props.isOpenDropBox === true ? '160px' : '0')};
+  transition: 0.5s;
+`;
 
 export const FieldListBox = styled.div`
   width: 16%;
@@ -19,11 +24,12 @@ export const FieldListBox = styled.div`
 
   & > div {
     width: 100%;
-    height: 125px;
+    height: 100%;
     border-bottom: 1px solid rgba(0, 0, 0, 0.7);
     display: flex;
     justify-content: center;
     align-items: center;
+    transition: 0.5s;
 
     span {
       font-size: 2rem;
@@ -32,6 +38,16 @@ export const FieldListBox = styled.div`
   }
 `;
 
-export const LightSidePromiseWrapper = styled.div`
+const SidePromiseWrapper = styled.div`
   width: 42%;
+  height: 100%;
+  background-color: #f6f6f6;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+  box-sizing: border-box;
 `;
+
+export const LightSidePromiseWrapper = styled(SidePromiseWrapper)``;
+
+export const LeftSidePromiseWrapper = styled(SidePromiseWrapper)``;
