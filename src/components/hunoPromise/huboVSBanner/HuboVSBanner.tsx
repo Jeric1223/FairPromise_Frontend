aa88a21e8a51b66b9acf4a79eb1a8abd73a200b6
JaeHyun <soehd0889@naver.com>
2022-06-03 11:19:19 +0900

    코드 리팩토링

--- src/components/hunoPromise/huboVSBanner/HuboVSBanner.tsx
+++ src/components/hunoPromise/huboVSBanner/HuboVSBanner.tsx
@@ -1,10 +1,8 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { VS } from 'assets/index';
 import * as S from './style';
 import HuboVSModal from 'components/hunoPromise/huboVSModal/index';
-import { useRecoilValue } from 'recoil';
-import { IsChangeHubo } from 'module/atom/huboDetail/huboDetail';
 import LeftVSContainer from '../huboVSContainer/LeftVSContainer';
 import RightVSContainer from '../huboVSContainer/RightVSContainer';
 
@@ -12,17 +10,9 @@ interface Props {}
 
 const HuboVSBanner: FC<Props> = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [reverse, setReverse] = useState(false);
-  const isChangeHubo = useRecoilValue(IsChangeHubo);
 
   let hubo_1 = searchParams.get('hubo_1');
   let hubo_2 = searchParams.get('hubo_2');
-
-  useEffect(() => {
-    if (hubo_1 > hubo_2) {
-      setReverse(true);
-    }
-  }, [hubo_1, hubo_2, searchParams, isChangeHubo]);
 
   return (
     <>
