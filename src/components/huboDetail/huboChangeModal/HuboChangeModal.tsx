4ace388b7d2117cefefbd8a1cc3d9a010f3e4d07
JaeHyun <soehd0889@naver.com>
2022-11-14 16:34:21 +0900

    build error fix

--- src/components/huboDetail/huboChangeModal/HuboChangeModal.tsx
+++ src/components/huboDetail/huboChangeModal/HuboChangeModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FC, useEffect } from 'react';
 import * as S from './style';
 import { HuboChangeModalIsOpen } from 'module/atom/huboDetail/huboDetail';
